1f55f1d58a6efa4e7ecfac5bc58c7d6e9b3425fd
xmdnx <smcartix@gmail.com>
2024-07-05 17:54:24 +0300

    replaces in tray

--- Telegram/SourceFiles/tray.cpp
+++ Telegram/SourceFiles/tray.cpp
@@ -7,6 +7,8 @@ https://github.com/rabbitgramdesktop/rabbitgramdesktop/blob/dev/LEGAL
 */
 #include "tray.h"
 
+#include "rabbit/lang/rabbit_lang.h"
+
 #include "core/application.h"
 #include "core/core_settings.h"
 #include "platform/platform_notifications_manager.h"
@@ -74,7 +76,7 @@ void Tray::rebuildMenu() {
 			_activeForTrayIconAction = Core::App().isActiveForTrayMenu();
 			return _activeForTrayIconAction
 				? tr::lng_minimize_to_tray(tr::now)
-				: tr::lng_open_from_tray(tr::now);
+				: ktr("rtg_open");
 		});
 
 		_tray.addAction(
@@ -95,7 +97,7 @@ void Tray::rebuildMenu() {
 			[=] { toggleSoundNotifications(); });
 	}
 
-	_tray.addAction(tr::lng_quit_from_tray(), [] { Core::Quit(); });
+	_tray.addAction(rktr("rtg_close"), [] { Core::Quit(); });
 
 	updateMenuText();
 }
