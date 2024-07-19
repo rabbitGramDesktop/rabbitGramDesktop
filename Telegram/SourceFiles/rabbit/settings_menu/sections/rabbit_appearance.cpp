d7a880d5f9fb0ab590fe74b45339b99dda9a8134
xmdnx <smcartix@gmail.com>
2024-07-19 22:45:54 +0300

    preview

--- Telegram/SourceFiles/rabbit/settings_menu/sections/rabbit_appearance.cpp
+++ Telegram/SourceFiles/rabbit/settings_menu/sections/rabbit_appearance.cpp
@@ -11,6 +11,7 @@ https://github.com/rabbitgramdesktop/rabbitgramdesktop/blob/dev/LEGAL
 #include "rabbit/lang/rabbit_lang.h"
 #include "rabbit/settings_menu/sections/rabbit_appearance.h"
 #include "rabbit/ui/settings/icon_picker.h"
+#include "rabbit/ui/settings/previews.h"
 
 #include "lang_auto.h"
 #include "mainwindow.h"
@@ -72,6 +73,10 @@ namespace Settings {
 	}
 
 	void RabbitAppearance::SetupAppearance(not_null<Ui::VerticalLayout *> container) {
+		const auto roundnessPreview = container->add(
+			object_ptr<RoundnessPreview>(container),
+			st::defaultSubsectionTitlePadding);
+
 		Ui::AddSubsectionTitle(container, rktr("rtg_settings_appearance"));
 
 		const auto userpicRoundnessLabel = container->add(
@@ -90,6 +95,7 @@ namespace Settings {
     	};
     	const auto updateUserpicRoundness = [=](int value) {
     		updateUserpicRoundnessLabel(value);
+			roundnessPreview->repaint();
     		::RabbitSettings::JsonSettings::Set("userpic_roundness", value);
     		::RabbitSettings::JsonSettings::Write();
     	};
